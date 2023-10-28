26eaed132e89d235b3b48c629b7e7ddde17552f5
Mojahidul Islam <bdemon245@gmail.com>
2023-10-28 18:23:11 +0000

    aded sail

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
 
         \App\Models\User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'admin@gmail.com',
         ]);
     }
 }
